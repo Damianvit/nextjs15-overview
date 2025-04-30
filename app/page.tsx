1b7ae95fe0a5381b415c46c4d20d3ea347d5b7d2
Damianvit <damianvitchib@gmail.com>
2025-04-30 07:24:25 +0100

    main code commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    < div className="flex flex-col  p-4 items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl font-bold">Welcome to the Home Page</h1>
+     </div>
+  );
+}
